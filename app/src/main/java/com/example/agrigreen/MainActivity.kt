7de050d3b1448d25bfc1a736932e1e772559a676
Priyanshumishra6696 <146465748+Priyanshumishra6696@users.noreply.github.com>
2025-03-13 18:14:22 +0530

    small changes

--- app/src/main/java/com/example/agrigreen/MainActivity.kt
+++ app/src/main/java/com/example/agrigreen/MainActivity.kt
@@ -4,18 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.ViewModelProvider
 import com.example.agrigreen.navigation.AgriGreenNav
-import com.example.agrigreen.ui.screens.LoginScreen
-import com.example.agrigreen.ui.screens.SignUpScreen
-import com.example.agrigreen.ui.screens.WelcomeScreen
 import com.example.agrigreen.ui.theme.AgriGreenTheme
 
 class MainActivity : ComponentActivity() {
